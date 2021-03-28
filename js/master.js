ff455d2eea44d66561e8acbcd25325e51ac8bc44
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2021-03-28 03:14:09 -0300

    Cambiados archivos binarios: ahora están en remoto. Añadido .gitignore Añadido Twitch en index.html

--- js/master.js
+++ js/master.js
@@ -4,11 +4,11 @@ let wallImg = document.querySelector(".wallImg")
 
 const randomWall = () => {
     if (wallpaper >= 0 && wallpaper <= 33.3) {
-        wallImg.setAttribute("src", "img/code-1839406_1920.jpg")
+        wallImg.setAttribute("src", "https://i.ibb.co/56j9xS1/code-1839406-1920.jpg")
     } else if (wallpaper >= 33.3 && wallpaper <= 66.6) {
-        wallImg.setAttribute("src", "img/technology-1283624_1280 (1).jpg")
+        wallImg.setAttribute("src", "https://i.ibb.co/jgmVg71/coding-1853305-1280.jpg")
     } else {
-        wallImg.setAttribute("src", "img/coding-1853305_1280.jpg")
+        wallImg.setAttribute("src", "https://i.ibb.co/23dDvGs/technology-1283624-1280-1.jpg")
     }
 }
 
