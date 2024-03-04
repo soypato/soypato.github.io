fedbe56867b16878c48e5e911ef51fb372c5b936
soypato <soypato>
2024-03-03 22:18:10 -0300

    Update titles and alt attributes in index.html

--- js/gallery.js
+++ js/gallery.js
@@ -2,206 +2,233 @@ const proyectos = [
     {
         "image": {
             "src": "img/FitHub-logo.webp",
-            "alt": "Logo de FitHub"
+            "alt": "Logo de FitHub",
+            "title": "Logo de FitHub"
         },
         "name": "FitHub Gimnasios",
         "description": "Sistema de Gimnasios en CLI con eficiencia de datos. Proyecto Final UTN - Lab. 2. Hecho en C.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/fithub"
+                "link": "https://github.com/soypato/fithub",
+                "title": "Enlace a GitHub"
             },
             {
                 "name": "Documentación",
-                "link": "https://github.com/soypato/FitHub/tree/main/aa%20docs%20project"
+                "link": "https://github.com/soypato/FitHub/tree/main/aa%20docs%20project",
+                "title": "Enlace a GitHub"
             }
         ]
     },
     {
         "image": {
             "src": "/img/stay.webp",
-            "alt": "Logo de Stay Track Gestiones"
+            "alt": "Logo de Stay Track Gestiones",
+            "title": "Logo de Stay Track Gestiones"
         },
         "name": "StayTrack Gestiones",
         "description": "Sistema de Hotel basado en consola, CRUD. Proyecto Final UTN - Lab. 1. Hecho en C.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/Stay-Track-Gestiones"
+                "link": "https://github.com/soypato/Stay-Track-Gestiones",
+                "title": "Enlace a GitHub"
             },
             {
                 "name": "Documentación",
-                "link": "https://github.com/soypato/Stay-Track-Gestiones/tree/main/OFFCODE%20ASSETS"
+                "link": "https://github.com/soypato/Stay-Track-Gestiones/tree/main/OFFCODE%20ASSETS",
+                "title": "Enlace a GitHub"
             }
         ]
     },
     {
         "image": {
             "src": "/img/duckTime.webp",
-            "alt": "Página principal de DuckTime"
+            "alt": "Página principal de DuckTime",
+            "title": "Página principal de DuckTime"
         },
         "name": "DuckTime",
         "description": "Una librería escrita en C para el manejo básico de fechas con sus respectivos comprobantes.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/ducktime"
+                "link": "https://github.com/soypato/ducktime",
+                "title": "Enlace a GitHub"
             },
             {
                 "name": "Instrucciones",
-                "link": "https://github.com/soypato/DuckTime/blob/main/readme.md"
+                "link": "https://github.com/soypato/DuckTime/blob/main/readme.md",
+                "title": "Enlace a GitHub"
             }
         ]
     },
     {
         "image": {
             "src": "/img/countrycodefinder.webp",
-            "alt": "Página principal de CountryCodeFinder"
+            "alt": "Página principal de CountryCodeFinder",
+            "title": "Página principal de CountryCodeFinder"
         },
         "name": "CountryCodeFinder",
         "description": "Buscador de países en base a su código de área. Hecho en Flask y SQL.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/CountryCodeFinder"
+                "link": "https://github.com/soypato/CountryCodeFinder",
+                "title": "Enlace a GitHub"
             },
             {
                 "name": "Documentación",
-                "link": "https://github.com/soypato/CountryCodeFinder/blob/main/readme.md"
+                "link": "https://github.com/soypato/CountryCodeFinder/blob/main/readme.md",
+                "title": "Enlace a GitHub"
             }
         ]
     },
     {
         "image": {
             "src": "img/EnExterior-banner.webp",
-            "alt": "Página principal de EnExterior"
+            "alt": "Página principal de EnExterior",
+            "title": "Página principal de EnExterior"
         },
         "name": "EnExterior",
         "description": "Valor de una compra en monedas extranjeras. Hecho con JavaScript y API de DolarSI.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/enexterior"
+                "link": "https://github.com/soypato/enexterior",
+                "title": "Enlace a GitHub"
             },
             {
                 "name": "Ver Demo",
-                "link": "soypato.github.io/enexterior"
+                "link": "soypato.github.io/enexterior",
+                "title": "Enlace a la demostración en GitHub Pages"
             }
         ]
     },
     {
         "image": {
             "src": "img/portfolio.webp",
-            "alt": "Vista previa del portfolio"
+            "alt": "Vista previa del portfolio",
+            "title": "Vista previa del portfolio"
         },
         "name": "Portfolio de soypato",
         "description": "¡Este portfolio! Realizado con HTML, CSS, JS y Bootstrap. El lugar donde viven mis proyectos.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/soypato.github.io/"
+                "link": "https://github.com/soypato/soypato.github.io/",
+                "title": "Enlace a GitHub"
             }
         ]
     },
     {
         "image": {
             "src": "img/clon-lt.webp",
-            "alt": "Captura del clon de Linktree"
+            "alt": "Captura del clon de Linktree",
+            "title": "Captura del clon de Linktree"
         },
         "name": "Clon de Linktree",
         "description": "Clon de recopilador de redes para social media. Hecho con HTML y CSS.",
         "buttons": [
             {
                 "name": "Código",
-                "link": "https://github.com/soypato/redes"
+                "link": "https://github.com/soypato/redes",
+                "title": "Enlace a GitHub"
             },
             {
                 "name": "Ver Demo",
-                "link": "bit.ly/soypato"
+                "link": "bit.ly/soypato",
+                "title": "Enlace a la demostración en GH Pages"
             }
         ]
     }
 ];
-
 const conferences = [
     {
         "image": {
             "src": "https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/d5d21a5d62e16fe6c131_soypato_19830465946_2014052701/thumb/custom-25dc52a3-be32-40df-b5c5-4a0409610145-320x180.png",
-            "alt": "Miniatura de clase de código"
+            "alt": "Miniatura de clase de código",
+            "title": "Miniatura de clase de código"
         },
         "name": "Clase introductoria de programación",
         "description": "Clase introductoria de lógica de programación y sistemas.",
         "buttons": [
             {
                 "name": "Ver Clase",
-                "link": "https://www.twitch.tv/videos/2078610954"
+                "link": "https://www.twitch.tv/videos/2078610954",
+                "title": "Enlace a Twitch"
             },  
             {
                 "name": "Temario",
-                "link": "https://mdputneduar-my.sharepoint.com/:w:/g/personal/patriciotubio_estudiante_mdp_utn_edu_ar/EX1Uc0SDQj1NjsqlGu75INQBQB8T2_F3f4prz3-IwWYW8A"
+                "link": "https://mdputneduar-my.sharepoint.com/:w:/g/personal/patriciotubio_estudiante_mdp_utn_edu_ar/EX1Uc0SDQj1NjsqlGu75INQBQB8T2_F3f4prz3-IwWYW8A",
+                "title": "Enlace al temario"
             }
         ]
     },
     {
         "image": {
             "src": "https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/22e2c462f423e5102e5e_soypato_33175249687_9860609536/thumb/custom-11723de6-0580-4fb2-a495-62f23b556d3d-320x180.png",
-            "alt": "Miniatura: Optimizar Windows"
+            "alt": "Miniatura: Optimizar Windows",
+            "title": "Miniatura: Optimizar Windows"
         },
         "name": "Optimización de Windows",
         "description": "Utilizamos scripts de debloating y personalización en Windows.",
         "buttons": [
             {
                 "name": "Ver Clase",
-                "link": "https://www.twitch.tv/videos/1712538272"
+                "link": "https://www.twitch.tv/videos/1712538272",
+                "title": "Enlace a Twitch"
             }
         ]
     },
     {
         "image": {
             "src": "https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/9740dc4adbcdb11b5fe3_soypato_22117433040_1801466352/thumb/custom-7adb0597-9a7e-46cc-9477-88bbb1a7fdeb-320x180.png",
-            "alt": "web scraping, hacking, instagram, osintgram, osint, hacking etico, soypato"
+            "alt": "web scraping, hacking, instagram, osintgram, osint, hacking etico, soypato",
+            "title": "web scraping, hacking, instagram, osintgram, osint, hacking etico, soypato"
         },
         "name": "Scraping y OSINT ético en Instagram",
         "description": "Clase educativa hacking en Instagram con Osintgram en Linux.",
         "buttons": [
             {
                 "name": "Ver Clase",
-                "link": "https://www.twitch.tv/videos/1619106308"
+                "link": "https://www.twitch.tv/videos/1619106308",
+                "title": "Enlace a Twitch"
             }
         ]
     },
     {
         "image": {
             "src": "https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/ad4dc23b3e324e18934c_soypato_58895367835_9739168497/thumb/custom-019d469d-f8f1-42f1-9feb-8761cad03b05-320x180.png",
-            "alt": "inteligencia artificial, foto, soypato"
+            "alt": "inteligencia artificial, foto, soypato",
+            "title": "inteligencia artificial, foto, soypato"
         },
         "name": "DALL·E: Inteligencia Artificial a fotos",
         "description": "Presentación de DALL·E, repercusiones y vistazo a la IA.",
         "buttons": [
             {
                 "name": "Ver Clase",
-                "link": "https://www.twitch.tv/videos/1503142718?filter=highlights&sort=time"
+                "link": "https://www.twitch.tv/videos/1503142718?filter=highlights&sort=time",
+                "title": "Enlace a Twitch"
             }
         ]
     },
     {
         "image": {
             "src": "https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/c4d53b5c00e4486aea5b_soypato_11673922893_9962255400/thumb/custom-d945b667-27ea-4587-b5a9-247d2a74585d-320x180.png",
-            "alt": "miniatura de pizarra con explicación, soypato"
+            "alt": "miniatura de pizarra con explicación, soypato",
+            "title": "miniatura de pizarra con explicación, soypato"
         },
         "name": "Guía básica sobre contraseñas",
         "description": "Cómo proteger nuestros datos en internet.",
         "buttons": [
             {
                 "name": "Ver Clase",
-                "link": "https://www.twitch.tv/videos/1476174774?filter=highlights&sort=time"
+                "link": "https://www.twitch.tv/videos/1476174774?filter=highlights&sort=time",
+                "title": "Enlace a Twitch"
             }
         ]
     }
-]
-
-
-
+];
 
 
 function generarGaleria(proyectos) {
@@ -216,6 +243,7 @@ function generarGaleria(proyectos) {
         image.setAttribute('loading', 'lazy');
         image.setAttribute('src', proyecto.image.src);
         image.setAttribute('alt', proyecto.image.alt);
+        image.setAttribute('title', proyecto.image.title);
 
         const projectName = document.createElement('h3');
         projectName.textContent = proyecto.name;
@@ -231,12 +259,14 @@ function generarGaleria(proyectos) {
             button.classList.add('fullWidthButton');
             button.setAttribute('href', proyecto.buttons[i].link);
             button.setAttribute('target', '_blank');
+            button.setAttribute('title', proyecto.buttons[i].title);
             button.innerHTML = `<span>${proyecto.buttons[i].name}</span>`;
             middleGridButtons.appendChild(button);
         }
 
         middleGrid.classList.add('middleGrid');
         middleGrid.appendChild(image);
+        middleGrid.appendChild(middleGridButtons);
         middleGrid.appendChild(projectName);
         middleGrid.appendChild(projectDescription);
         middleGrid.appendChild(middleGridButtons);
